6d6b81c0ea0b1ffe18d68c01f8616756f51f4cf2
Che-Julia <121030128+Che-Julia@users.noreply.github.com>
2023-05-01 19:44:33 +0300

    Update CardDeliveryTest.java

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -18,7 +18,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class CardDeliveryTest {
     @BeforeEach
     void setup() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999/");
     }
 
     @Test
